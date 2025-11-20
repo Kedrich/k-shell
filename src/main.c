34d6471cd856168018ac4ec5c2c0d12cba9672ea
Kedrich <kedrich.custodio@gmail.com>
2025-11-20 19:17:44 +0800

    Feat: Added built-in cd command using chdir()

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define MAX_LINE_LENGTH 256
 #define MAX_ARGS 64
@@ -11,11 +13,11 @@ int main(void)
     char line[MAX_LINE_LENGTH];
     char *args[MAX_ARGS];
 
-    while(1)
+    while (1)
     {
         printf("k-shell> ");
 
-        if(fgets(line, MAX_LINE_LENGTH, stdin) == NULL)
+        if (fgets(line, MAX_LINE_LENGTH, stdin) == NULL)
         {
             printf("\nGoodbye Sir/Madam!\n");
             break;
@@ -26,7 +28,7 @@ int main(void)
         int i = 0;
         char *token = strtok(line, " \t");
 
-        while(token != NULL)
+        while (token != NULL)
         {
             args[i] = token;
             i++;
@@ -36,12 +38,47 @@ int main(void)
 
         args[i] = NULL;
 
-        printf("DEBUG: Found %d tokens:\n", i);
-        for(int j = 0; j < i; j++)
+        if (args[0] == NULL)
+            continue;
+        
+        if (strcmp(args[0], "exit") == 0)
         {
-            printf("  args[%d]: %s\n", j, args[j]);
+            printf("Exiting k-shell....\n");
+            break;
         }
 
+        else if (strcmp(args[0], "cd") == 0)
+        {
+            if (args[1] == NULL)
+            {
+                fprintf(stderr, "k-shell: expected argument to \"cd\"\n");
+            }
+            else
+            {
+                if(chdir(args[1]) != 0)
+                {
+                    perror("k-shell");
+                }
+            }
+        }
+
+        else
+        {
+            pid_t pid = fork();
+
+            if (pid == 0)
+            {
+                if(execvp(args[0], args) == -1)
+                {
+                    perror("k-shell");
+                }
+                exit(EXIT_FAILURE);
+            }
+            else if (pid < 0)
+                perror("k-shell");
+            else
+                wait(NULL);
+        }
     }
 
 
