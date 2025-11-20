d175da8a399f10a34bab562edf5d5eb2d42d436b
Kedrich <kedrich.custodio@gmail.com>
2025-11-20 19:05:43 +0800

    Feat: Implement tokenizer to parse command arguments

--- src/main.c
+++ src/main.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 
 #define MAX_LINE_LENGTH 256
+#define MAX_ARGS 64
 
 int main(void)
 {
 
     char line[MAX_LINE_LENGTH];
+    char *args[MAX_ARGS];
 
     while(1)
     {
@@ -21,7 +23,25 @@ int main(void)
 
         line[strcspn(line, "\n")] = 0;
 
-        printf("DEBUG: You typed %s\n", line);
+        int i = 0;
+        char *token = strtok(line, " \t");
+
+        while(token != NULL)
+        {
+            args[i] = token;
+            i++;
+
+            token = strtok(NULL, " \t");
+        }
+
+        args[i] = NULL;
+
+        printf("DEBUG: Found %d tokens:\n", i);
+        for(int j = 0; j < i; j++)
+        {
+            printf("  args[%d]: %s\n", j, args[j]);
+        }
+
     }
 
 
